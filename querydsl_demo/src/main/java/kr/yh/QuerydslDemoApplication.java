621e6c0f517f0bb47e13ae5931f4d24672b3dfeb
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-12-20 16:10:23 +0900

    feat: mysql demo 추가

---
+++ querydsl_demo/src/main/java/kr/yh/QuerydslDemoApplication.java
@@ -0,0 +1,13 @@
+package kr.yh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydslDemoApplication.class, args);
+    }
+
+}
